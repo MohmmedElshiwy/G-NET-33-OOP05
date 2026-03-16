201887738aef1d943e7b4f2a9cf89a3655b9d4d5
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2026-03-16 17:39:06 +0300

    Solve the first problem from the first part

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace G_NET_33_OOP_5;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        #region Theoretical Questions
+
+        //Q1 : What is an interface in C#? Why do we use interfaces instead of depending on concrete classes directly? Mention at least three benefits of using interfaces.
+
+        /*
+        
+        -- An interface in C# is a contract that defines a set of methods, properties, or events without providing their implementation. Any class that implements the interface must provide the implementation for all its members.
+
+
+        -- Interfaces help make the code more flexible, maintainable, and loosely coupled. Instead of depending on a specific class, the code depends on a general contract.
+        
+        
+        -- 1- Loose Coupling
+           2- Multiple Implementations
+           3- Better Testability
+           4- Supports Dependency Injection
+        */
+
+
+       
+
+
+
+        #endregion
+    }
+}
